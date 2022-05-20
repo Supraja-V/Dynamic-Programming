6e821d2a7da9c518fb5cbe7e83cc0e52a23dd540
supr1234 <82039719+supr1234@users.noreply.github.com>
2022-05-21 00:56:14 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 1e5+2;
+int dp[N];
+int fib(int n)
+{
+    if(n<=2)
+    return 1;
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    dp[n] = fib(n-1)+fib(n-2);
+    return dp[n];
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i=0;i<N;i++)
+        dp[i] = -1;
+    cout << fib(n) << endl;
+}
+
+
